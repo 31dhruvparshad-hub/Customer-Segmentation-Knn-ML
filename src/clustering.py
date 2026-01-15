e63da0ee1460396694296fd15ae4a0b18c24a74e
Dhruv Parshad <31dhruvparshad@gmail.com>
2026-01-15 22:37:12 +0530

    Create clustering.py

---
+++ src/clustering.py
@@ -0,0 +1,10 @@
+from sklearn.cluster import KMeans
+
+def run_kmeans(X, n_clusters=5):
+    model = KMeans(
+        n_clusters=n_clusters,
+        random_state=42,
+        n_init=10
+    )
+    labels = model.fit_predict(X)
+    return model, labels
